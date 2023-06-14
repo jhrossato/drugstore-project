82ab4da6d64f306acd4c3a17d870db397bd030b2
Joao H Rossato <johrossato@gmail.com>
2023-06-14 00:44:27 -0300

    route configurations

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+// let medicamentosPage = document.querySelector('#medicamentos-page');
+// let mainContent = document.querySelector('#main-page');
+
+// medicamentosPage.addEventListener('click', async function(event){
+//     event.preventDefault();
+//     const response = await fetch('/src/view/home/home.html')
+//     .then(function (response) {
+//         console.log(response)
+//       return response.text();
+//     });
+//     mainContent.innerHTML = response;
+// });
